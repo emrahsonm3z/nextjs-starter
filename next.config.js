55b5dea9eff4728b4dc72fb04583c4e6d059a56b
emrah sonmez <emrah.sonmez@itsbilisim.com>
2021-05-27 11:53:05 +0300

    Enabled webpack bundle analyzer.

--- next.config.js
+++ next.config.js
@@ -4,8 +4,12 @@ require('dotenv').config()
 const withPlugins = require('next-compose-plugins')
 const withPWA = require('next-pwa')
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
 module.exports = withPlugins(
-  [],
+  [[withBundleAnalyzer]],
   withPWA({
     pwa: {
       dest: 'public',
